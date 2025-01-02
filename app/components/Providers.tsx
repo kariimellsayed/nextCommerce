c9c44b27ab084a0ba93953797959b79b8aad0163
kariimellsayed <kariimellsayed@gmail.com>
2025-01-02 14:41:51 +0200

    edit stripe links

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CaProvider({ children }: { children: ReactNode }) {
       mode="subscription" // إذا كنت تستخدم اشتراكات متكررة
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="https://next-commerce-rose.vercel.app/stripe/success"
-      cancelUrl="https://next-commerce-rose.vercel.app/stripe/error"
+      successUrl="https://next-commerce-rose.vercel.app/success"
+      cancelUrl="https://next-commerce-rose.vercel.app/error"
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
